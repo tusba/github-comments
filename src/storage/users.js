0d7e2d99db19f8768a141a93078bb117662f8033
tusba <11007086+tusba@users.noreply.github.com>
2021-08-28 21:51:01 +0600

    Add user data storage

---
+++ src/storage/users.js
@@ -0,0 +1,51 @@
+const { UserData: UserDataModel } = require('../models')
+
+/**
+ * @class UserData
+ * @classdesc Storage for information about user's comments and commits
+ */
+module.exports = class UserData {
+    constructor() {
+        /**
+         * Map object to store user info
+         *
+         * @type Map<Number, UserDataModel>
+         */
+        this.map = new Map()
+    }
+
+    /**
+     * Add comment to user
+     *
+     * @param {Number} userId - User ID to add comment to
+     * @param {String} userLogin - Optional user login name
+     * @returns this
+     */
+    addComment(userId, userLogin = undefined) {
+        const isNewEntry = !this.map.has(userId)
+        const userData = isNewEntry ? new UserDataModel(userId) : this.map.get(userId)
+
+        userLogin && (userData.login = userLogin)
+        userData.commentCount++
+        isNewEntry && this.map.set(userId, userData)
+
+        return this
+    }
+
+    /**
+     * Get user data as sorted list:
+     * a) by number of comments - descending
+     * b) by user login name - ascending
+     *
+     * @returns UserDataModel[]
+     */
+    get orderedList() {
+        return [...this.map.values()].sort((a, b) => {
+            if (a.commentCount !== b.commentCount) {
+                return b.commentCount - a.commentCount
+            }
+
+            return a.login < b.login ? -1 : 1
+        })
+    }
+}
