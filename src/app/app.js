6a10ab96fe3314087f7e8c0f7212650d3f1d7397
tusba <11007086+tusba@users.noreply.github.com>
2021-09-02 21:15:24 +0600

    After third eslint fix

--- src/app/app.js
+++ src/app/app.js
@@ -1,5 +1,8 @@
 const chalk = require('chalk')
-const { Comment: CommentModel, Contribution: ContributionModel } = require('../models')
+const {
+  Comment: CommentModel,
+  Contribution: ContributionModel,
+} = require('../models')
 const Requests = require('../request')
 
 /**
@@ -7,176 +10,189 @@ const Requests = require('../request')
  * @classdesc Application wrapper
  */
 module.exports = class App {
+  /**
+   * @param {String} repo - Repository ID
+   */
+  constructor(repo) {
     /**
-     * @param {String} repo - Repository ID
-     */
-    constructor(repo) {
-        /**
-         * Repository ID
-         *
-         * @type String
-         */
-        this.repo = repo
-        /**
-         * Number of days
-         *
-         * @type Number
-         */
-        this.periodDays = undefined
-    }
-
-    /**
-     * @return {Date}
-     */
-    get limitDate() {
-        if (!this.periodDays) {
-            return undefined
-        }
-
-        const d = new Date()
-        d.setDate(d.getDate() - this.periodDays)
-
-        return d
-    }
-
-    /**
-     * @param {String} value 
-     * @throws
-     */
-    set period(value) {
-        const match = /^(\d+)d$/i.exec(value)
-        if (match === null) {
-            throw new Error('Wrong format of period')
-        }
-
-        this.periodDays = parseInt(match[1])
-        if (!this.periodDays) {
-            throw new Error('Wrong value of period')
-        }
-    }
-
-    /**
-     * Make requests continouosly and transform response data
+     * Repository ID
      *
-     * @param {Generator} response - Generator with request result
-     * @param {(Object) => Comment} convert - Callback to convert response data to comment model
-     * @param {(Object) => Boolean} filter - Optional callback to filter response data before converting
-     * @yields Comment
-     * @throws
+     * @type String
      */
-    async *processResponse(response, convert, filter = undefined) {
-        try {
-            for await (const result of response) {
-                if (!result.data.length) {
-                    // if data array is empty then it's done
-                    return
-                }
+    this.repo = repo
+    /**
+     * Number of days
+     *
+     * @type Number
+     */
+    this.periodDays = undefined
+  }
 
-                const responseData = typeof filter === 'function' ? result.data.filter(filter) : result.data
+  /**
+   * @return {Date}
+   */
+  get limitDate() {
+    if (!this.periodDays) {
+      return undefined
+    }
 
-                yield responseData.map(convert)
-            }
-        } catch (err) {
-            console.error(chalk.red('Request processing failed:'), err.message)
-            throw err
+    const d = new Date()
+    d.setDate(d.getDate() - this.periodDays)
+
+    return d
+  }
+
+  /**
+   * @param {String} value
+   * @throws
+   */
+  set period(value) {
+    const match = /^(\d+)d$/i.exec(value)
+    if (match === null) {
+      throw new Error('Wrong format of period')
+    }
+
+    this.periodDays = parseInt(match[1], 10)
+    if (!this.periodDays) {
+      throw new Error('Wrong value of period')
+    }
+  }
+
+  /**
+   * Make requests continouosly and transform response data
+   *
+   * @param {Generator} response - Generator with request result
+   * @param {(Object) => Comment} convert - Callback to convert response data to comment model
+   * @param {(Object) => Boolean} filter - Optional callback to filter response data before converting
+   * @yields Comment
+   * @throws
+   */
+  async *processResponse(response, convert, filter = undefined) {
+    try {
+      for await (const result of response) {
+        if (!result.data.length) {
+          // if data array is empty then it's done
+          return
         }
+
+        const responseData =
+          typeof filter === 'function'
+            ? result.data.filter(filter)
+            : result.data
+
+        yield responseData.map(convert)
+      }
+    } catch (err) {
+      console.error(chalk.red('Request processing failed:'), err.message)
+      throw err
+    }
+  }
+
+  /**
+   * Fetch comment data from many sources
+   *
+   * @param {(Array) => void} treat - Callback to process an array of obtained comments
+   */
+  async fetchComments(treat) {
+    // date to limit comments from results
+    const limitDate = this.limitDate
+      ?.toISOString()
+      .replace(/\.\d{3}(?=Z$)/i, '')
+
+    /**
+     * Callback to convert response data to comment model
+     *
+     * @param {Object} responseData
+     * @returns Comment
+     */
+    const cbConvert = ({ id, user, created_at: createdAt }) => {
+      const comment = new CommentModel()
+
+      comment.id = id
+      comment.createdAt = createdAt
+      comment.author.id = user?.id || null
+      comment.author.login = user?.login || null
+
+      return comment
     }
 
     /**
-     * Fetch comment data from many sources
+     * Optional callback to filter response data before converting
      *
-     * @param {(Array) => void} treat - Callback to process an array of obtained comments
+     * @param {Object} responseData
+     * @returns Boolean
      */
-    async fetchComments(treat) {
-        // date to limit comments from results
-        const limitDate = this.limitDate?.toISOString().replace(/\.\d{3}(?=Z$)/i, '')
-
-        /**
-         * Callback to convert response data to comment model
-         *
-         * @param {Object} responseData
-         * @returns Comment
-         */
-        const cbConvert = ({ id, user, created_at }) => {
-            const comment = new CommentModel()
-
-            comment.id = id
-            comment.createdAt = created_at
-            comment.author.id = user?.id || null
-            comment.author.login = user?.login || null
-
-            return comment
-        }
-
-        /**
-         * Optional callback to filter response data before converting
-         *
-         * @param {Object} responseData
-         * @returns Boolean
-         */
-        const cbFilter = limitDate ? ({ created_at }) => created_at >= limitDate : undefined
-
-        /**
-         * Options to filter via query params
-         */
-        const queryFilter = {
-            filter: false,
-            queryParams: limitDate ? { since: limitDate } : undefined
-        }
-
-        // description for requests
-        const requests = [
-            // repo comments
-            { class: Requests.Repo },
-            // issue comments
-            { class: Requests.Issue, ...queryFilter },
-            // pull comments
-            { class: Requests.Pull, ...queryFilter }
-        ]
-
-        for (const request of requests) {
-            try {
-                const response = await new request.class(this.repo).fetch(request.queryParams)
-                const results = this.processResponse(
-                    response,
-                    request.convert || cbConvert,
-                    request.filter === false ? undefined : (request.filter || cbFilter)
-                )
-
-                for await (const dataItems of results) {
-                    // dataItems is an array of objects returned by request.convert or cbConvert callback
-                    treat(dataItems)
-                }
-            } catch (err) {
-                console.error(chalk.red('Comment fetching failed:'), err.message)
-            }
-        }
-    }
+    const cbFilter = limitDate
+      ? ({ created_at: createdAt }) => createdAt >= limitDate
+      : undefined
 
     /**
-     * Fetch contribution activity data
-     *
-     * @param {(ContributionModel) => void} treat - Callback to process an obtained contribution
+     * Options to filter via query params
      */
-     async fetchContributions(treat) {
-        try {
-            const response = await new Requests.Contribution(this.repo).fetch()
-
-            for await (const result of response) {
-                const contributions = result.data.map(({ total, author }) => {
-                    const contribution = new ContributionModel()
-
-                    contribution.commitCount = total
-                    contribution.author.id = author?.id || null
-                    contribution.author.login = author?.login || null
-
-                    return contribution
-                })
-
-                contributions.forEach(treat)
-            }
-        } catch (err) {
-            console.error(chalk.red('Contribution fetching response failed:'), err.message)
-        }
+    const queryFilter = {
+      filter: false,
+      queryParams: limitDate ? { since: limitDate } : undefined,
     }
+
+    // description for requests
+    const requests = [
+      // repo comments
+      { className: Requests.Repo },
+      // issue comments
+      { className: Requests.Issue, ...queryFilter },
+      // pull comments
+      { className: Requests.Pull, ...queryFilter },
+    ]
+
+    for (const request of requests) {
+      try {
+        const RequestClass = request.className
+        const response = await new RequestClass(this.repo).fetch(
+          request.queryParams,
+        )
+        const results = this.processResponse(
+          response,
+          request.convert || cbConvert,
+          request.filter === false ? undefined : request.filter || cbFilter,
+        )
+
+        for await (const dataItems of results) {
+          // dataItems is an array of objects returned by request.convert or cbConvert callback
+          treat(dataItems)
+        }
+      } catch (err) {
+        console.error(chalk.red('Comment fetching failed:'), err.message)
+      }
+    }
+  }
+
+  /**
+   * Fetch contribution activity data
+   *
+   * @param {(ContributionModel) => void} treat - Callback to process an obtained contribution
+   */
+  async fetchContributions(treat) {
+    try {
+      const response = await new Requests.Contribution(this.repo).fetch()
+
+      for await (const result of response) {
+        const contributions = result.data.map(({ total, author }) => {
+          const contribution = new ContributionModel()
+
+          contribution.commitCount = total
+          contribution.author.id = author?.id || null
+          contribution.author.login = author?.login || null
+
+          return contribution
+        })
+
+        contributions.forEach(treat)
+      }
+    } catch (err) {
+      console.error(
+        chalk.red('Contribution fetching response failed:'),
+        err.message,
+      )
+    }
+  }
 }
--- src/index.js
+++ src/index.js
@@ -7,60 +7,64 @@ const { Users: UserStorage } = require('./storage')
 
 // parse command line options
 program
-    .requiredOption('--repo <id>', 'repository ID is required')
-    .option('--period <interval>', 'number of days followed by "d"')
-    .parse()
+  .requiredOption('--repo <id>', 'repository ID is required')
+  .option('--period <interval>', 'number of days followed by "d"')
+  .parse()
 
 const { repo: repoId, period: days } = program.opts()
 
 // initialize application
 const app = new App(repoId)
 try {
-    days && (app.period = days)
+  days && (app.period = days)
 } catch (err) {
-    console.error(chalk.red(err.message))
-    process.exit(1)
+  console.error(chalk.red(err.message))
+  process.exit(1)
 }
 
 // inform about user input before starting
 {
-    const strPeriod = app.periodDays ? ` for past ${app.periodDays} days` : ''
-    console.log('\n  ' + chalk.white(`Fetching comments${strPeriod} for "${app.repo}"...`))
+  const strPeriod = app.periodDays ? ` for past ${app.periodDays} days` : ''
+  console.log(
+    '\n  ' + chalk.white(`Fetching comments${strPeriod} for "${app.repo}"...`),
+  )
 }
 
 const rate = {
-    initialized: false,
-    limit: undefined,
-    remaining: undefined,
-    used: undefined
+  initialized: false,
+  limit: undefined,
+  remaining: undefined,
+  used: undefined,
 }
 
 AppEvents.Emitter.on(AppEvents.Enum.rateLimit, ({ limit, remaining, used }) => {
-    rate.initialized = true
+  rate.initialized = true
 
-    rate.limit = limit
-    rate.remaining = remaining
-    rate.used = used
+  rate.limit = limit
+  rate.remaining = remaining
+  rate.used = used
 
-    if (!remaining) {
-        console.error(chalk.red('Rate limit is exceeded'))
-        process.exit(2)
-    }
+  if (!remaining) {
+    console.error(chalk.red('Rate limit is exceeded'))
+    process.exit(2)
+  }
 })
 
 async function initProgressIndicator() {
-    const ora = (await import('ora')).default({
-        spinner: 'bouncingBar', // on windows always 'line'
-        prefixText: '\n '
-    })
+  const ora = (await import('ora')).default({
+    spinner: 'bouncingBar', // on windows always 'line'
+    prefixText: '\n ',
+  })
 
-    AppEvents.Emitter.on(AppEvents.Enum.beforeRequest, description => {
-        const { used, limit } = rate
-        const strRate = rate.initialized ? ` [${used} request(s) of ${limit} used]` : ''
-        ora.text = `${description}${strRate}`
-    })
+  AppEvents.Emitter.on(AppEvents.Enum.beforeRequest, (description) => {
+    const { used, limit } = rate
+    const strRate = rate.initialized
+      ? ` [${used} request(s) of ${limit} used]`
+      : ''
+    ora.text = `${description}${strRate}`
+  })
 
-    return ora
+  return ora
 }
 
 /**
@@ -69,47 +73,63 @@ async function initProgressIndicator() {
  * @returns UserDataModel[]
  */
 async function run() {
-    const userData = new UserStorage()
+  const userData = new UserStorage()
 
-    // make request to fetch comments and group them by author
-    await app.fetchComments(/** @param {Comment[]} comments */ comments => {
-        for (const { author: { id, login } } of comments) {
-            userData.addComment(id, login)
-        }
-    })
+  // make request to fetch comments and group them by author
+  await app.fetchComments(
+    /** @param {Comment[]} comments */
+    (comments) => {
+      // eslint-disable-next-line prettier/prettier
+      for (const { author: { id, login } } of comments) {
+        userData.addComment(id, login)
+      }
+    },
+  )
 
-    if (!userData.size) {
-        return []
-    }
+  if (!userData.size) {
+    return []
+  }
 
-    // make request to fetch contribution activity and add it to authors
-    await app.fetchContributions(/** @param {Contribution} */ ({ author: { id }, commitCount }) => {
-        userData.addContribution(id, commitCount)
-    })
+  // make request to fetch contribution activity and add it to authors
+  await app.fetchContributions(
+    /** @param {Contribution} */
+    ({ author: { id }, commitCount }) => {
+      userData.addContribution(id, commitCount)
+    },
+  )
 
-    return userData.orderedList
+  return userData.orderedList
 }
 
 async function main() {
-    const progressIndicator = (await initProgressIndicator()).start('\n')
-    await fetchRateLimit() // initialize shared HTTP client
-    const results = await run()
-    progressIndicator.succeed('Done\n')
+  const progressIndicator = (await initProgressIndicator()).start('\n')
+  await fetchRateLimit() // initialize shared HTTP client
+  const results = await run()
+  progressIndicator.succeed('Done\n')
 
-    if (!results.length) {
-        console.log(chalk.white('No data available'))
-        return
-    }
+  if (!results.length) {
+    console.log(chalk.white('No data available'))
+    return
+  }
 
-    // output sorted and formatted results
-    const maxCommentCountLength = String(results[0].commentCount).length
+  // output sorted and formatted results
+  const maxCommentCountLength = String(results[0].commentCount).length
 
-    results.forEach(/** @param {UserDataModel[]} info */ info => {
-        const strCommentCount = String(info.commentCount).padStart(maxCommentCountLength)
-        const strLogin = info.login || 'N/A'
-        const strCommitCount = info.login ? ` (${info.commitCount} commits)` : ''
-        console.log(chalk.white(`${strCommentCount} comments, ${strLogin}${strCommitCount}`))
-    })
+  results.forEach(
+    /** @param {UserDataModel[]} info */
+    (info) => {
+      const strCommentCount = String(info.commentCount).padStart(
+        maxCommentCountLength,
+      )
+      const strLogin = info.login || 'N/A'
+      const strCommitCount = info.login ? ` (${info.commitCount} commits)` : ''
+      console.log(
+        chalk.white(
+          `${strCommentCount} comments, ${strLogin}${strCommitCount}`,
+        ),
+      )
+    },
+  )
 }
 
 main()
